17dffb0e9a080c55e50fc2fceecf419e92c64c0e
OluwapelumiG <ainaoluwapelumigideon@gmail.com>
2025-05-26 12:36:57 +0100

    Changed project name

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ export default function Navbar() {
         <nav className="bg-gray-800 p-4">
             <div className="container mx-auto flex justify-between items-center">
                 <div className="text-white font-bold text-xl">
-                    MY APP
+                    OUT TUTORIAL APP
                 </div>
                 <div className="space-x-4">
                     <a href="/" className="text-white hover:text-gray-300">Home</a>
